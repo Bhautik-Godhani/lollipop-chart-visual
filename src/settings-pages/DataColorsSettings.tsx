10ddd6c1c36e49098e88fa9ebc298a137295598f
Bhautik Godhani <Bhautik@Truviz.ai>
2024-05-31 14:03:40 +0530

    When we have Others and we apply color by category, then don't give seperate option for Others, its already in category list

--- src/settings-pages/DataColorsSettings.tsx
+++ src/settings-pages/DataColorsSettings.tsx
@@ -525,7 +525,7 @@ const DataColors = (props) => {
 		setConfigValues((d) => {
 			return {
 				...d,
-				[EDataColorsSettings.CategoryColors]: [...shadow.categoriesColorList].filter((v, i, a) => a.findIndex((t) => t.name === v.name) === i)
+				[EDataColorsSettings.CategoryColors]: [...shadow.categoriesColorList].filter(d => !d.name.includes(shadow.othersLabel)).filter((v, i, a) => a.findIndex((t) => t.name === v.name) === i)
 			};
 		});
 
@@ -533,7 +533,7 @@ const DataColors = (props) => {
 		setConfigValues((d) => {
 			return {
 				...d,
-				[EDataColorsSettings.SubCategoryColors]: [...shadow.subCategoriesColorList].filter((v, i, a) => a.findIndex((t) => t.name === v.name) === i),
+				[EDataColorsSettings.SubCategoryColors]: [...shadow.subCategoriesColorList].filter(d => !d.name.includes(shadow.othersLabel)).filter((v, i, a) => a.findIndex((t) => t.name === v.name) === i),
 			};
 		});
 	}, [configValues.fillType]);
