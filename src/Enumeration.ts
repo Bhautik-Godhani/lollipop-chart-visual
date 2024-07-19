fef5079c02611e5c3309575fa9ebf83c6fe3b1a1
Bhautik Godhani <Bhautik@Truviz.ai>
2024-07-19 17:35:20 +0530

    Legends are not getting turned off even after disabling from menu

--- src/Enumeration.ts
+++ src/Enumeration.ts
@@ -229,7 +229,7 @@ function getYAxisConfigSelection(): EnumerateSectionType {
 
 function getLegendSelection(): EnumerateSectionType {
 	return {
-		name: "Legend",
+		name: "legend",
 		isShow: true,
 		properties: [
 			{
