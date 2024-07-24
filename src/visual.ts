869283a4efd8cd66e44a05039a3ac712729e2fd5
Bhautik Godhani <Bhautik@Truviz.ai>
2024-07-24 19:33:10 +0530

    Small Multiple > when I have one panel with +ve and -ve data but rest have positive data > then color for marker is not coming correctly.

--- src/visual.ts
+++ src/visual.ts
@@ -2269,7 +2269,7 @@ export class Visual extends Shadow {
 		// 		});
 		// }
 
-		const categoricalDataValues = categoricalData.values.filter(d => d.source.roles[EDataRolesName.Measure]);
+		const categoricalDataValues = this.originalCategoricalData.values.filter(d => d.source.roles[EDataRolesName.Measure]);
 
 		const min = d3.min(categoricalDataValues, d => d3.min(d.values, v => <number>v));
 		const max = d3.max(categoricalDataValues, d => d3.max(d.values, v => <number>v));
