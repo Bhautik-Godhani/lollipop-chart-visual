73ea3be6574b8551c70aebc97d44dd9db0e2276d
Bhautik Godhani <Bhautik@Truviz.ai>
2024-08-23 12:56:06 +0530

    Conditional formatting > Ranking > sub-category > bottom N > when I have applied bottom 2 it just highlight 1 category

--- src/visual.ts
+++ src/visual.ts
@@ -4437,7 +4437,7 @@ export class Visual extends Shadow {
 						}
 					}
 				} else if (c.rankingType === ECFRankingTypes.BottomN) {
-					if (i > ((this.categoricalDataPairs.length - 1) - c.staticRankingValue)) {
+					if (i >= ((this.categoricalDataPairs.length - 1) - c.staticRankingValue)) {
 						if (c.applyOnCategories.includes(ECFApplyOnCategories.Marker)) {
 							this.categoryColorPair[d.category].marker1Color = c.color;
 							this.categoryColorPair[d.category].marker2Color = c.color;
@@ -4484,7 +4484,7 @@ export class Visual extends Shadow {
 								}
 							}
 						} else if (c.rankingType === ECFRankingTypes.BottomN) {
-							if (i > ((d.subCategories.length - 1) - c.staticRankingValue)) {
+							if (i >= ((d.subCategories.length - 1) - c.staticRankingValue)) {
 								if (c.applyOnCategories.includes(ECFApplyOnCategories.Marker)) {
 									this.subCategoryColorPair[`${d.category}-${s.category}`].marker1Color = c.color;
 									this.subCategoryColorPair[`${d.category}-${s.category}`].marker2Color = c.color;
